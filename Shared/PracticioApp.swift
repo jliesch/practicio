2d51d8797927a351f9d74842c54ad06ef99e8502
jliesch <jliesch79@hotmail.com>
2022-08-09 09:00:27 -0700

    Simple cleanup, moved constants around

--- Shared/PracticioApp.swift
+++ Shared/PracticioApp.swift
@@ -7,15 +7,28 @@
 
 import SwiftUI
 
+// Sort order for practice items
 enum SortOrder: CaseIterable {
     case alphabetical, lastPracticed, frequency, score
+}
+
+struct Constants {
+    static var minFrequency = 0.1
+    static var avgFrequency = 1.0
+    static var maxFrequency = 10.0
+    
+    // Ranking for practice items that have never been practiced
+    static var defaultItemAge = 7
 }
 
 class AppState: ObservableObject {
     @Published var selectedCategory: Category? = nil
     @Published var selectedItem: Item? = nil
-    @Published var changedCounter: Int = 0
     @Published var sortOrder: SortOrder = .score
+
+    // changeCounter is used to force views to update when the underlying
+    // storage may have changed but thew view is not aware of it.
+    @Published var changedCounter: Int = 0
 }
 
 extension Text {
